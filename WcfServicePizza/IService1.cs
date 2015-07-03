6c3fe4d798af8943faf9815c9b6cd995fe90b824
Muhammad Rehan Qadri <muhammadrehanqadri@outlook.com>
2015-07-03 10:49:55 +0500

    1

---
+++ WcfServicePizza/IService1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServicePizza
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace="http://pizzaapp.apphb.com/")]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(UriTemplate="SignUp",
+            Method="POST",
+            BodyStyle=WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat=WebMessageFormat.Json,
+            RequestFormat=WebMessageFormat.Json)]
+        bool SignUp(Customer customer);
+
+        [OperationContract]
+        [WebGet(UriTemplate="Login/{phoneNo}/{password}",
+            BodyStyle=WebMessageBodyStyle.WrappedRequest,
+            RequestFormat=WebMessageFormat.Json,
+            ResponseFormat=WebMessageFormat.Json)]
+        bool Login(string phoneNo, string password);
+
+        [OperationContract]
+        [WebInvoke(Method="POST", BodyStyle=WebMessageBodyStyle.WrappedRequest,RequestFormat=WebMessageFormat.Json,ResponseFormat=WebMessageFormat.Json,UriTemplate="CreatePizza")]
+        bool CreatePizza(Pizza pizza);
+
+        [OperationContract]
+        [WebInvoke(Method="POST",BodyStyle=WebMessageBodyStyle.WrappedRequest, RequestFormat=WebMessageFormat.Json,ResponseFormat=WebMessageFormat.Json, UriTemplate="OrderPizza")]
+        bool OrderPizza(Order order, List<OrderDetail> orderDetails);
+
+        [OperationContract]
+        [WebGet(BodyStyle=WebMessageBodyStyle.WrappedRequest, RequestFormat=WebMessageFormat.Json, ResponseFormat=WebMessageFormat.Json,UriTemplate="GetAllOrders")] 
+        List<Order> GetAllOrders();
+
+        [OperationContract]
+        [WebGet(BodyStyle=WebMessageBodyStyle.WrappedRequest, RequestFormat=WebMessageFormat.Json, ResponseFormat=WebMessageFormat.Json,UriTemplate="GetOrderDetails/{orderId}")]
+        List<OrderDetail> GetOrderDetails(string orderId);
+        
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
+
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
+}
---
+++ WcfServicePizza/Service1.svc.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServicePizza
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+        [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1 : IService1
+    {
+
+        public bool SignUp(Customer customer)
+        {
+            try
+            {
+                DatabasePizzaEntities db = new DatabasePizzaEntities();
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool Login(string phoneNo, string password)
+        {
+            decimal phoneNoDecimal = Decimal.Parse(phoneNo);
+            DatabasePizzaEntities db = new DatabasePizzaEntities();
+            Customer customer = db.Customers.Where(c => c.PhoneNo==phoneNoDecimal && c.Password == password).SingleOrDefault();
+            if (customer == null)
+                return false;
+            return true;
+        }
+
+        public bool CreatePizza(Pizza pizza)
+        {
+            try
+            {
+                DatabasePizzaEntities db = new DatabasePizzaEntities();
+                db.Pizzas.Add(pizza);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool OrderPizza(Order order, List<OrderDetail> orderDetails)
+        {
+            try
+            {
+                DatabasePizzaEntities db = new DatabasePizzaEntities();
+                db.Orders.Add(order);
+                foreach (var orderDetail in orderDetails)
+                {
+                    db.OrderDetails.Add(orderDetail);
+                }
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public List<Order> GetAllOrders()
+        {
+            DatabasePizzaEntities db = new DatabasePizzaEntities();
+            var orders = db.Orders;
+            List<Order> ordersList = null;
+            if (orders != null)
+            {
+                ordersList = new List<Order>();
+            }
+            foreach (var order in orders)
+            {
+                ordersList.Add(order);
+            }
+            return ordersList;
+        }
+
+
+        public List<OrderDetail> GetOrderDetails(string orderId)
+        {
+            int orderIdInt = Int32.Parse(orderId);
+            DatabasePizzaEntities db = new DatabasePizzaEntities();
+            var orderDetails = db.OrderDetails.Where(o => o.OrderId == orderIdInt);
+            List<OrderDetail> orderDetailsList = null;
+            if (orderDetails != null)
+            {
+                orderDetailsList = new List<OrderDetail>();
+            }
+            foreach (var order in orderDetails)
+            {
+                orderDetailsList.Add(order);
+            }
+            return orderDetailsList;
+        }
+    }
+}
